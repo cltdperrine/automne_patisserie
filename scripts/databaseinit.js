c6ca19ca37b923804b8cde5610e5b13de76698c2
cltdperrine <cltd.perrine@gmail.com>
2026-04-14 11:55:16 +0200

    update schema to uuid

--- scripts/databaseinit.js
+++ scripts/databaseinit.js
@@ -12,8 +12,11 @@ if (!databaseUrl) {
 const databaseClient = neon(databaseUrl);
 
 async function createDatabase() {
+  await databaseClient`
+  CREATE EXTENSION IF NOT EXISTS "pgcrypto"`;
+
   await databaseClient`CREATE TABLE IF NOT EXISTS users (
-      id SERIAL PRIMARY KEY,
+      id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
       first_name TEXT NOT NULL,
       last_name TEXT NOT NULL,
       email TEXT UNIQUE NOT NULL,
@@ -22,7 +25,7 @@ async function createDatabase() {
     )`;
 
   await databaseClient`CREATE TABLE IF NOT EXISTS products (
-      id SERIAL PRIMARY KEY,
+      id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
       name TEXT NOT NULL,
       price NUMERIC(10,2) NOT NULL,
       description TEXT,
@@ -31,7 +34,7 @@ async function createDatabase() {
     )`;
 
   await databaseClient`CREATE TABLE IF NOT EXISTS categories (
-      id SERIAL PRIMARY KEY,
+      id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
       name TEXT NOT NULL,
       image TEXT NOT NULL,
       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
@@ -44,44 +47,44 @@ async function createDatabase() {
   await databaseClient`CREATE TYPE status AS ENUM ('pending', 'canceled', 'fulfilled')`;
 
   await databaseClient`CREATE TABLE IF NOT EXISTS orders (
-      id SERIAL PRIMARY KEY,
-      client_id INT REFERENCES users(id) ON DELETE CASCADE,
+      id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+      client_id UUID REFERENCES users(id) ON DELETE CASCADE,
       status status NOT NULL DEFAULT 'pending',
       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     )`;
 
   await databaseClient`CREATE TABLE IF NOT EXISTS images (
-      id SERIAL PRIMARY KEY,
+      id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
       name TEXT,
       url TEXT,
-      product_id INT REFERENCES products(id) ON DELETE CASCADE,
+      product_id UUID REFERENCES products(id) ON DELETE CASCADE,
       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     )`;
 
   await databaseClient`
     ALTER TABLE images
-    ADD COLUMN IF NOT EXISTS product_id INT REFERENCES products(id) ON DELETE CASCADE`;
+    ADD COLUMN IF NOT EXISTS product_id UUID REFERENCES products(id) ON DELETE CASCADE`;
 
   await databaseClient`DROP TABLE IF EXISTS cart CASCADE`;
 
   await databaseClient`CREATE TABLE cart (
-      product_id INT NOT NULL REFERENCES products(id) ON DELETE CASCADE,
-      user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+      product_id UUID NOT NULL REFERENCES products(id),
+      user_id UUID NOT NULL REFERENCES users(id),
       quantity INT NOT NULL DEFAULT 1 CHECK (quantity > 0),
       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
       PRIMARY KEY (product_id, user_id)
     )`;
 
   await databaseClient`CREATE TABLE IF NOT EXISTS order_products (
-      order_id INT NOT NULL REFERENCES orders(id) ON DELETE CASCADE,
-      product_id INT NOT NULL REFERENCES products(id) ON DELETE CASCADE,
+      order_id UUID NOT NULL REFERENCES orders(id) ON DELETE CASCADE,
+      product_id UUID NOT NULL REFERENCES products(id) ON DELETE CASCADE,
       quantity INT NOT NULL DEFAULT 1 CHECK (quantity > 0),
       PRIMARY KEY (order_id, product_id)
     )`;
 
   await databaseClient`CREATE TABLE IF NOT EXISTS product_categories (
-      product_id INT NOT NULL REFERENCES products(id) ON DELETE CASCADE,
-      category_id INT NOT NULL REFERENCES categories(id) ON DELETE CASCADE,
+      product_id UUID NOT NULL REFERENCES products(id) ON DELETE CASCADE,
+      category_id UUID NOT NULL REFERENCES categories(id) ON DELETE CASCADE,
       PRIMARY KEY (product_id, category_id)
     )`;
 }
