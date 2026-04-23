c68a525cac6d8ca1a9cdd5265d8974c0c6b937c2
cltdperrine <cltd.perrine@gmail.com>
2026-04-23 09:53:24 +0200

    feat: add get images list endpoint and controller

--- src/api/images/controller/index.js
+++ src/api/images/controller/index.js
@@ -1,10 +1,12 @@
 import deleteImage from "./delete-image.js";
 import getImage from "./get-image.js";
+import getImages from "./get-images.js";
 import patchImage from "./patch-image.js";
 import createImage from "./post-image.js";
 
 const imagesController = {
   create: createImage,
+  getAll: getImages,
   get: getImage,
   patch: patchImage,
   delete: deleteImage,
