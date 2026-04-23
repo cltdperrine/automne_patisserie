c58fd793578fe428272db75ba0ee66d28939daf3
cltdperrine <cltd.perrine@gmail.com>
2026-04-23 09:45:05 +0200

    feat: add get orders list endpoint and controller

--- src/api/orders/controllers/index.js
+++ src/api/orders/controllers/index.js
@@ -1,10 +1,12 @@
 import deleteOrder from "./delete-order.js";
 import getOrder from "./get-order.js";
+import getOrders from "./get-orders.js";
 import patchOrder from "./patch-order.js";
 import createOrder from "./post-order.js";
 
 const ordersController = {
   create: createOrder,
+  getAll: getOrders,
   get: getOrder,
   patch: patchOrder,
   delete: deleteOrder,
--- src/api/orders/routes.js
+++ src/api/orders/routes.js
@@ -4,6 +4,7 @@ import ordersController from "./controllers/index.js";
 const ordersRouter = Router();
 
 ordersRouter.post("/", ordersController.create);
+ordersRouter.get("/", ordersController.getAll);
 ordersRouter.get("/:id", ordersController.get);
 ordersRouter.patch("/:id", ordersController.patch);
 ordersRouter.delete("/:id", ordersController.delete);
