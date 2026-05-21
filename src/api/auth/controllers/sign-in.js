5b9efdcbb533c7fd0e925453d6c94bb4843cbe73
perrinecltd <cltd.perrine@gmail.com>
2026-05-21 17:46:25 +0200

    feat: auth middleware

--- src/api/auth/controllers/sign-in.js
+++ src/api/auth/controllers/sign-in.js
@@ -5,10 +5,8 @@ import jwt from "jsonwebtoken";
 const SIGNATURE = process.env.JWT_SECRET;
 
 export default async function signIn(req, res) {
-  // get email and password sent by the user
   const { email, password } = req.body;
 
-  // get the user from the db using the email
   const [user] = await databaseClient`
   SELECT * FROM users WHERE email = ${email}`;
 
@@ -16,14 +14,11 @@ export default async function signIn(req, res) {
     return res.status(401).json("Invalid credentials");
   }
 
-  // compare the password with the hashed password
   const isValid = await bcrypt.compare(password, user.password);
-  // if the passwords don't match, the request is rejected
   if (isValid === false) {
     return res.status(401).json("Invalid password");
   }
 
-  //build a payload without the password
   const payload = {
     id: user.id,
     first_name: user.first_name,
@@ -31,7 +26,6 @@ export default async function signIn(req, res) {
     email: user.email,
   };
 
-  // sign a token
   const token = jwt.sign(payload, SIGNATURE);
 
   res.cookie("token", token, {
@@ -41,6 +35,5 @@ export default async function signIn(req, res) {
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
-  //send back the user data with the token
   return res.status(200).json({ user: payload, token });
 }
--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import apiRouter from "./api/index.js";
 import cookieParser from "cookie-parser";
+import authMiddleware from "./middlewares/authMiddleware.js";
 dotenv.config();
 
 const CLIENT_URL = process.env.CLIENT_URL;
@@ -12,6 +13,7 @@ const app = express();
 app.use(cors({ origin: CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
+app.use(authMiddleware);
 app.use("/api", apiRouter);
 
 app.get("/health", (req, res) => {
